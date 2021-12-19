792b1b3c66658cbfa2850466e0685f29870f00da
queil <queil@users.noreply.github.com>
2021-12-19 23:34:01 +0000

    Make it work with relative paths + some more love for cli args

--- src/Queil.Gmox.DotNet.Cli/Cli.fs
+++ src/Queil.Gmox.DotNet.Cli/Cli.fs
@@ -4,32 +4,39 @@ open Argu
 open System.IO
 
 type CliArgs =
-| Proto of string list
-| ProtoRoot of string
-| Import_Path of string list
-| Stubs_Dir of string
+| [<AltCommandLine("-p")>]Proto_Paths of string list
+| [<AltCommandLine("-r")>]Proto_Root_Dir of string
+| [<AltCommandLine("-i")>]Import_Paths of string list
+| [<AltCommandLine("-s")>]Stubs_Dir of string
+| [<AltCommandLine("-w")>]Work_Dir of string
 with
   interface IArgParserTemplate with
     member this.Usage =
       match this with
-      | Proto _ -> "gRPC service proto path."
-      | ProtoRoot _ -> "Root directory if operating in a monorepo."
-      | Import_Path _ -> "Additional protos import path(s)."
+      | Proto_Paths _ -> "Proto paths to compile"
+      | Proto_Root_Dir _ -> "Proto Root directory if operating in a monorepo."
+      | Import_Paths _ -> "Additional protos import path(s)."
       | Stubs_Dir _ -> "Directory containing stubs definitions to pre-load."
+      | Work_Dir _ -> "If specified overrides the current working directory."
 
 type Options = {
   Proto: string list
   ProtoRoot: string
   ImportPaths: string list
-  StubsDir: string
+  StubsDir: string option
+  WorkDir: string
 }
 
   let parseOptions argv =
     let parser = ArgumentParser.Create<CliArgs>(programName = "gmox")
+    
     let cmd = parser.ParseCommandLine(inputs = argv, raiseOnUsage = true)
+    let cwd = cmd.TryGetResult(Work_Dir) |> Option.defaultValue (Directory.GetCurrentDirectory()) |> Path.TrimEndingDirectorySeparator
+    let fullPath (path:string) = Path.Combine(cwd, path |> Path.TrimEndingDirectorySeparator)
     {
-      Proto = cmd.GetResult(Proto) |> Seq.map Path.TrimEndingDirectorySeparator |> Seq.toList
-      ProtoRoot = cmd.GetResult(ProtoRoot) |> Path.TrimEndingDirectorySeparator
-      ImportPaths = cmd.GetResult(Import_Path) |> Seq.map Path.TrimEndingDirectorySeparator |> Seq.toList
-      StubsDir = cmd.GetResult(Stubs_Dir) |> Path.TrimEndingDirectorySeparator
+      Proto = cmd.GetResult(Proto_Paths) |> Seq.map fullPath |> Seq.toList
+      ProtoRoot = cmd.GetResult(Proto_Root_Dir) |> fullPath
+      ImportPaths = cmd.GetResult(Import_Paths) |> Seq.map fullPath |> Seq.toList
+      StubsDir = cmd.TryGetResult(Stubs_Dir) |> Option.map(fullPath)
+      WorkDir = cwd
     }
