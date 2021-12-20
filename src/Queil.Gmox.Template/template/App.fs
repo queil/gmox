bd55200527c8516b69256a26c19e5a9152a4e2d0
queil <queil@users.noreply.github.com>
2021-12-20 10:02:18 +0000

    Initial work for stub pre-loading

--- src/Queil.Gmox.Template/template/App.fs
+++ src/Queil.Gmox.Template/template/App.fs
@@ -9,12 +9,17 @@ open Queil.Gmox.Core.Types
 open Microsoft.AspNetCore.Routing
 open Microsoft.AspNetCore.Server.Kestrel.Core
 open Microsoft.Extensions.DependencyInjection
+open Microsoft.Extensions.Hosting
 open Saturn
 open System
+open System.IO
 open System.Reflection
 open System.Text.Json
 open System.Text.Json.Serialization
 open System.Runtime.Serialization
+open System.Threading.Tasks
+
+type StubPreloader = IHostedService
 
 let router =
   router {
@@ -44,24 +49,20 @@ let router =
         })
     }
 
-#if (standalone)
-let app (services: Type list) =
-#else
-let app =
-#endif
+type AppSettings = {
+  Services: Type list
+  StubPreloadDir: string
+}
+
+let app (config: AppSettings) =
+
   application {
     listen_local 4770 (fun opts -> opts.Protocols <- HttpProtocols.Http2)
     listen_local 4771 (fun opts -> opts.Protocols <- HttpProtocols.Http1)
     memory_cache
     use_gzip
     use_dynamic_grpc [
-#if (standalone)
-      yield! services
-#else
-      yield! (
-        Queil.Gmox.Infra.Grpc.servicesFromAssemblyOf<Grpc.Health.V1.Health> |> Seq.map Emit.makeImpl
-      )
-#endif
+      yield! config.Services
     ]
     use_router router
     service_config (fun svcs ->
@@ -81,9 +82,35 @@ let app =
             |> Seq.find(fun x -> x.Method.FullName.[1..] = s.Method)
           method.ServiceType.GetMethod(s.Method.Split("/")[1], BindingFlags.Public ||| BindingFlags.Instance)
       )) |> ignore
-      svcs.AddSingleton<StubStore>()
+      svcs.AddSingleton<StubStore>() |> ignore
+      svcs.AddSingleton<StubPreloader>(fun f ->
+        {
+          new StubPreloader with
+            override _.StartAsync(_: Threading.CancellationToken): Task = 
+              task {
+                let serializer = f.GetRequiredService<Json.ISerializer>()
+                let store = f.GetRequiredService<StubStore>()
+                do! (
+                  Directory.EnumerateFiles(config.StubPreloadDir, "*.json")
+                  |> Seq.map(fun path ->
+                      task {
+                        use stream = File.OpenRead(path)
+                        let! stubs = serializer.DeserializeAsync<Stub []>(stream)
+                        for s in stubs do
+                          store.addOrReplace s
+                      }) 
+                  |> Seq.map (fun x -> x :> Task)
+                  |> Seq.toArray
+                  |> Task.WhenAll
+                )
+              }
+            override _.StopAsync(_: Threading.CancellationToken): Task = Task.CompletedTask
+        })
     )
   }
 #if (!standalone)
-run app
+run (app {
+  Services = Infra.Grpc.servicesFromAssemblyOf<Grpc.Health.V1.Health> |> Seq.map Emit.makeImpl |> Seq.toList
+  StubPreloadDir = "stubs"
+})
 #endif
